6beb5f5b222eec411aa8c878e1b39a6e607491d1
kiizerd <ajsewell13@gmail.com>
2021-08-17 16:57:21 -0500

    passed tests for #make_all_pieces

--- lib/factory.rb
+++ lib/factory.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require_relative 'pieces/pieces'
 require_relative 'event_bus/event_bus'
 
@@ -5,105 +6,71 @@ require_relative 'event_bus/event_bus'
 # PieceFactory NodeFactory
 class Factory
 
-  @@counts =  {
-    white: {
-      pawn: 0,
-      bishop: 0,
-      knight: 0,
-      rook: 0,
-      king: 0,
-      queen: 0
-    },
-    black: {
-      pawn: 0,
-      bishop: 0,
-      knight: 0,
-      rook: 0,
-      king: 0,
-      queen: 0
-    }
-  }
+  @@amounts = { pawn: 8, bishop: 2, knight: 2, rook: 2, queen: 1, king: 1 }
 
-  @@pieces = {
-    white: {
+  # TODO - make this prettier
+  def self.make_all_pieces
+    [:white, :black].reduce({}) do |pieces, color|
+      pieces.merge(color => (@@amounts.map do |type, count|
+        typed_pieces = case
+        when type == :king  then make_piece(color, :king)
+        when type == :queen then make_piece(color, :queen)
+        else Array.new(count) { |i| make_piece(color, type, i) }
+        end
+        [type, typed_pieces]
+      end.to_h))
+    end
+  end
 
-    },
-    black: {
-
-    }
-  }
-
-  def self.make_piece(color, type)
-    piece   = make_typed_piece(color, type)
-    payload = { piece: piece, color: color }
+  def self.make_piece(color, type, count=0)
+    piece   = make_typed_piece(color, type, count)
+    payload = { color: color, piece: piece, number: count }
     EventBus.publish(:piece_made, payload) # listen for this event at player and at node
     piece
   end
 
-  def self.make_typed_piece(color, type)
+  def self.make_typed_piece(color, type, num=0)
     case
-    when type == :pawn   then make_pawn(color)
-    when type == :bishop then make_bishop(color)
-    when type == :knight then make_knight(color)
-    when type == :rook   then make_rook(color)
+    when type == :pawn   then make_pawn(color, num)
+    when type == :bishop then make_bishop(color, num)
+    when type == :knight then make_knight(color, num)
+    when type == :rook   then make_rook(color, num)
     when type == :queen  then make_queen(color)
     when type == :king   then make_king(color)
     end
   end
 
-  def self.make_pawn color
-    count = counts[color][:pawn]
-    return pieces[color][:pawn][-1] if count == 8
+  def self.make_pawn color, num
     rank = color == :black ? 6 : 1
-    file = count
+    file = num
     Pawn.new(color, [rank, file])
   end
 
-  def self.make_bishop color
-    count = counts[color][:bishop]
-    return pieces[color][:bishop][1] if count == 2
+  def self.make_bishop color, num
     rank = color == :black ? 7 : 0
-    file = count == 0   ? 2 : 5
+    file = num   == 0   ? 2 : 5
     Bishop.new(color, [rank, file])
   end
 
-  def self.make_knight color
-    count = counts[color][:knight]
-    return pieces[color][:knight][1] if count == 2
+  def self.make_knight color, num
     rank = color == :black ? 7 : 0
-    file = count == 0      ? 1 : 6
+    file = num   == 0      ? 1 : 6
     Knight.new(color, [rank, file])
   end
 
-  def self.make_rook color
-    count = counts[color][:rook]
-    return pieces[color][:rook][1] if count == 2
+  def self.make_rook color, num
     rank = color == :black ? 7 : 0
-    file = count == 0      ? 7 : 0
+    file = num   == 0      ? 7 : 0
     Rook.new(color, [rank, file])
   end
 
   def self.make_queen color
-    return pieces[color][:queen] if counts[color][:queen] == 1
     pos = color == :black ? [7, 3] : [0, 3]
-    counts[color][:queen] += 1
-    pieces[color][:queen] = Queen.new color, pos
+    Queen.new color, pos
   end
 
   def self.make_king color
-    return pieces[color][:king] if counts[color][:king] == 1
     pos = color == :black ? [7, 4] : [0, 4]
-    counts[color][:king] += 1
-    pieces[color][:king] = King.new color, pos
-  end
-
-  def self.counts
-    @@counts
-  end
-
-  private
-
-  def self.pieces
-    @@pieces
+    King.new color, pos
   end
 end
