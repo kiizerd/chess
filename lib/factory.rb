0bbf176590e5dfba0dbba2be7db6f9a594b96e81
kiizerd <ajsewell13@gmail.com>
2021-08-17 12:20:32 -0500

    passed first test for Factory class

--- lib/factory.rb
+++ lib/factory.rb
@@ -1,11 +1,33 @@
 require_relative 'pieces/pieces'
 require_relative 'event_bus/event_bus'
 
+# TODO - extract groupable methods into modules
+# PieceFactory NodeFactory
 class Factory
+
+  @@counts =  {
+    white: {
+      pawn: 0,
+      bishop: 0,
+      knight: 0,
+      rook: 0,
+      king: 0,
+      queen: 0
+    },
+    black: {
+      pawn: 0,
+      bishop: 0,
+      knight: 0,
+      rook: 0,
+      king: 0,
+      queen: 0
+    }
+  }
+
   def self.make_piece(color, type)
-    piece = make_typed_piece(color, type)
-    event_name = "piece_made_" + color.to_s + "_" + type.to_s 
-    EventBus.publish(event_name, piece)
+    piece   = make_typed_piece(color, type)
+    payload = { piece: piece, color: color }
+    EventBus.publish(:piece_made, payload) # listen for this event at player and at node
     piece
   end
 
@@ -21,27 +43,52 @@ class Factory
   end
 
   def self.make_pawn color
-
+    count = counts[color][:pawn]
+    return if count == 8
+    rank = color == :black ? 6 : 1
+    file = count
+    Pawn.new(color, [rank, file])
   end
 
   def self.make_bishop color
-
+    count = counts[color][:bishop]
+    return if count == 2
+    rank = color == :black ? 7 : 0
+    file = count == 0   ? 2 : 5
+    Bishop.new(color, [rank, file])
   end
 
   def self.make_knight color
-
+    count = counts[color][:knight]
+    return if count == 2
+    rank = color == :black ? 7 : 0
+    file = count == 0      ? 1 : 6
+    Knight.new(color, [rank, file])
   end
 
   def self.make_rook color
-
+    count = counts[color][:rook]
+    return if count == 2
+    rank = color == :black ? 7 : 0
+    file = count == 0      ? 7 : 0
+    Rook.new(color, [rank, file])
   end
 
   def self.make_queen color
-
+    return if counts[color][:queen] == 1
+    pos = color == :black ? [7, 3] : [0, 3]
+    @@counts[color][:queen] += 1
+    Queen.new color, pos
   end
 
   def self.make_king color
+    return if counts[color][:king] == 1
     pos = color == :black ? [7, 4] : [0, 4]
+    counts[color][:king] += 1
     King.new color, pos
+  end
+
+  def self.counts
+    @@counts
   end
 end
