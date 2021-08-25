0307725ae8ac987106751753e423cf4db073343b
kiizerd <ajsewell13@gmail.com>
2021-08-25 14:28:59 -0500

    added test for Board#can_castle?, Board#king_in_check?, and Board#king_in_checkmate?

--- spec/board/board_spec.rb
+++ spec/board/board_spec.rb
@@ -2,8 +2,10 @@ require_relative '../../lib/board/board'
 require_relative '../../lib/factory'
 
 describe Board do
+  subject(:board) { described_class.new }
+
   describe "#make_nodes" do
-    let(:nodes) { Board.make_nodes }  
+    let(:nodes) { board.make_nodes }  
 
     it "returns an hash" do
       expect(nodes).to be_a Hash
@@ -26,15 +28,15 @@ describe Board do
       let(:piece) { Factory.make_piece(:black, :king) }
 
       it "moves piece" do
-        Board.apply_move(piece, piece.pos, [6, 4])
+        board.apply_move(piece, piece.pos, [6, 4])
         expect(piece.pos).to eq [6, 4]
       end
 
       it "updates last move class var" do
         initial_pos = piece.pos
         dest_pos    = [6, 4]
-        Board.apply_move(piece, initial_pos, dest_pos)
-        expect(Board.last_move).to include(
+        board.apply_move(piece, initial_pos, dest_pos)
+        expect(board.last_move).to include(
           piece: piece,
           origin: initial_pos,
           dest: dest_pos
@@ -47,8 +49,8 @@ describe Board do
       let(:captured) { Factory.make_piece(:black, :pawn) }
 
       before do
-        Board.node_at([1, 3]).set_piece captured
-        Board.apply_move(attacker, attacker.pos, captured.pos)
+        board.node_at([1, 3]).update_piece captured
+        board.apply_move(attacker, attacker.pos, captured.pos)
       end
 
       it "moves attacker" do
@@ -63,19 +65,18 @@ describe Board do
 
   describe "#can_en_passant?" do
     context "given pawn can en passant" do
-      
       let(:attacking_pawn) { Factory.make_piece(:white, :pawn) }
       let(:captured_pawn)  { Factory.make_piece(:black, :pawn) }
       
       before do
         atk_origin = attacking_pawn.pos
         cpt_origin = captured_pawn.pos
-        Board.apply_move(attacking_pawn, atk_origin, [4, 0])
-        Board.apply_move(captured_pawn, cpt_origin, [4, 1])
+        board.apply_move(attacking_pawn, atk_origin, [4, 0])
+        board.apply_move(captured_pawn, cpt_origin, [4, 1])
       end
       
       it "returns true" do
-        en_passant = Board.can_en_passant? attacking_pawn
+        en_passant = board.can_en_passant? attacking_pawn
         expect(en_passant).to be true
       end
     end
@@ -84,10 +85,10 @@ describe Board do
       let(:piece)  { Factory.make_piece(:white, :pawn) }
       let(:knight) { Factory.make_piece(:black, :knight) }
 
-      before { Board.apply_move(knight, knight.pos, [5, 0]) }
+      before { board.apply_move(knight, knight.pos, [5, 0]) }
 
       it "returns false" do
-        en_passant = Board.can_en_passant? piece
+        en_passant = board.can_en_passant? piece
         expect(en_passant).to be false
       end
     end
@@ -96,28 +97,37 @@ describe Board do
       let(:piece) { Factory.make_piece(:white, :knight) }
 
       it "returns false" do
-        check = Board.can_en_passant? piece
+        check = board.can_en_passant? piece
         expect(check).to be false  
       end
     end
   end
 
   describe "#can_castle?" do
+    let(:king) { Factory.make_piece(:white, :king) }
+    let(:rook) { Factory.make_piece(:white, :rook) }
+
+    before do
+      board.instance_variable_set(:@nodes, board.make_nodes)
+      board.node_at(king.pos).update_piece king
+      board.node_at(rook.pos).update_piece rook
+    end
+
     context "given king rook pair that can castle" do
-      xit "returns true" do
-        
+      it "returns true" do
+        check = board.can_castle? king, rook
+        expect(check).to be true
       end
     end
 
     context "given king rook pair that cannot castle" do
-      xit "returns false" do
-        
+      before { Factory.make_all_pieces }
+
+      it "returns false" do
+        check = board.can_castle? king, rook
+        expect(check).to be false
       end
     end
-  end
-
-  describe "#whatif_move?" do
-    
   end
 
   describe "#king_in_check?" do
