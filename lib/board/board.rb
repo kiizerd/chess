7177a982d3762c5ee653e09892ad018b85d4fb82
kiizerd <ajsewell13@gmail.com>
2021-08-25 14:30:39 -0500

    methods to check castling, king in check and helpers

--- lib/board/board.rb
+++ lib/board/board.rb
@@ -2,40 +2,54 @@ require_relative 'node'
 require 'pry'
 
 class Board
-  @@ranks = (:a..:h).to_a
-  @@files = (1..8).to_a
 
-  @@last_move = {}
+  attr_reader :nodes, :last_move
 
-  def self.make_nodes
+  def initialize
+    @ranks = (1..8).to_a
+    @files = (:a..:h).to_a
+    @last_move = {}
+    @nodes = make_nodes
+  end
+
+  def make_nodes
     nodes = {}
-    @@ranks.each_with_index do |rank, ind|
-      @@files.each do |file|
-        symbol = "#{rank}#{file}".to_sym
-        nodes[symbol] = Node.new([ind, file - 1])
+    @files.each_with_index do |file, ind|
+      @ranks.each do |rank|
+        symbol = "#{file}#{rank}".to_sym
+        nodes[symbol] = Node.new([ind, rank - 1])
       end
     end
     nodes
   end
 
-  @@nodes = make_nodes
+  def pieces
+    @nodes
+      .values
+      .select { |n| n.occupied? }
+      .map    { |n| n.piece }
+      .each   { |p| yield p if block_given? }
+  end
+
+  def get_king player_color
+    pieces.select { |p| p.color == player_color && p.class == King }[0]
+  end
 
   # Applies move to piece without checking gamerules or move validity
-  def self.apply_move piece, origin, dest
-    origin_node = @@nodes[(get_node_key(origin[0], origin[1]))]
-    origin_node.remove_piece
-
-    dest_node = @@nodes[get_node_key(dest[0], dest[1])]
-    dest_node.piece_captured(piece) if dest_node.occupied?
-    dest_node.set_piece piece
-    @@last_move = { piece: piece, origin: origin, dest: dest }
+  def apply_move piece, origin, dest
+    node_at(origin).remove_piece
+    
+    dest_node = node_at(dest)
+    dest_node.piece_captured_by(piece) if dest_node.occupied?
+    dest_node.update_piece piece
+    @last_move = { piece: piece, origin: origin, dest: dest }
   end
 
-  def self.last_move
-    @@last_move
+  def last_move
+    @last_move
   end
 
-  def self.can_en_passant? attacker
+  def can_en_passant? attacker
     return false unless attacker.is_a? Pawn
     return false unless last_move[:piece].is_a? Pawn
     return false unless attacker.color != last_move[:piece].color
@@ -44,7 +58,7 @@ class Board
     return true
   end
 
-  def self.last_move_pawn_double?
+  def last_move_pawn_double?
     return false unless last_move[:piece].is_a? Pawn
     org  = last_move[:origin][0]
     dest = last_move[:dest][0]
@@ -52,7 +66,7 @@ class Board
     return false
   end
 
-  def self.attacker_was_passed? attacker
+  def attacker_was_passed? attacker
     atk_rank, atk_file = attacker.pos
     tgt_rank, tgt_file = last_move[:piece].pos
     same_rank = tgt_rank == atk_rank
@@ -62,34 +76,116 @@ class Board
   end
 
   def can_castle? king_piece, rook_piece
-
+    return false unless king_piece.prev.nil?
+    return false unless rook_piece.prev.nil?
+    king_node, rook_node = node_at(king_piece.pos), node_at(rook_piece.pos)
+    return false unless nodes_between_clear?(king_node, rook_node)
+    color = king_piece.color
+    return false unless nodes_between_safe?(king_node, rook_node, color)
+    return true
   end
 
-  def whatif_move?
-
+  def nodes_between_clear? node_a, node_b
+    get_nodes_between(node_a, node_b).none? { |n| n.occupied? }
   end
 
-  def king_in_check? player
-
+  def nodes_between_safe? node_a, node_b, safe_color
+    nodes = get_nodes_between(node_a, node_b)
+    nodes.all? { |n| node_safe?(n, safe_color) }
   end
 
-  def king_in_checkmate? player
-
+  def node_safe? node, safe_color
+    pieces
+      .reject { |p| p.color == safe_color }
+      .select { |p| p.get_valid_moves.values.include? node.pos }
+      .empty?
   end
 
-  def check_move
-    # temporarily applies the move to determine if executing
-    # said move will result in a players own king in check
-    # their opponents king in check, or if any enemy piece is
-    # occupying the players selected pieces destination node
+  def check_move? piece, origin, dest
+    real_nodes = stub_nodes
+
+    apply_move(piece, origin, dest)
+
+    check = king_in_check?(piece.color)
+    @nodes = real_nodes
+    return check
   end
 
-  def self.get_node_key row, col
-    "#{@@ranks[row]}#{@@files[col]}".to_sym
+  def king_in_check? player_color
+    king = get_king(player_color)
+    king_node = node_at king.pos
+    # inverted for clarity; king in check when node not safe
+    return node_safe?(king_node, player_color) ? false : true
   end
 
-  def self.node_at arr
+  def king_in_checkmate? player_color
+    return false unless king_in_check?(player_color)
+    possibles = get_king(player_color).get_valid_moves
+    return possibles.any? { |poss| node_safe?(node_at(poss)) }
+  end
+
+  def clone_nodes
+    Marshal.load(Marshal.dump(@nodes))
+  end
+
+  def stub_nodes
+    fake_nodes = clone_nodes
+    real_nodes = @nodes
+    @nodes = fake_nodes
+    return real_nodes
+  end
+
+  def get_node_key row, col
+    "#{@files[row]}#{@ranks[col]}".to_sym
+  end
+
+  def node_at arr
     row, col = arr
-    @@nodes[get_node_key(row, col)]
+    @nodes[get_node_key(row, col)]
+  end
+
+  def get_nodes_between node_a, node_b
+    case
+    when node_a.pos[1] == node_b.pos[1]
+      get_nodes_between_rank(node_a, node_b)
+    when node_a.pos[0] == node_b.pos[0]
+      get_nodes_between_file(node_a, node_b)
+    when node_a.pos[0] - node_b.pos[0] == node_a.pos[1] - node_b.pos[1]
+      get_nodes_between_diag(node_a, node_b)
+    else
+      false
+    end
+  end
+
+  def get_nodes_between_rank node_a, node_b
+    positions = [node_a.pos[0], node_b.pos[0]]
+    ranks = (positions.min..positions.max).to_a[1..-2]
+    file = node_a.pos[1]
+    ranks.reduce([]) do |nodes, rank|
+      nodes << node_at([rank, file])
+      nodes
+    end
+  end
+
+  def get_nodes_between_file node_a, node_b
+    positions = [node_a.pos[1], node_b.pos[1]]
+    files = (positions.min..positions.max).to_a[1..-2]
+    rank = node_a.pos[0]
+    files.reduce([]) do |nodes, file|
+      nodes << node_at([rank, file])
+      nodes
+    end
+  end
+
+  def get_nodes_between_diag node_a, node_b
+
   end
 end
+
+require_relative "../../lib/factory"
+
+board = Board.new
+
+Factory.make_all_pieces
+
+binding.pry
